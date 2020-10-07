a0958f2898b7564696e4864e9ee0cee4ea4fb734
Vicente Guzman <luciomsp@geeks.ms>
2020-10-07 13:18:02 -0500

    Improvements Ch5 & Ch6

--- Ch_5/CuriosidadesdelAjolote/lambda/src/utilities/languageStrings.js
+++ Ch_5/CuriosidadesdelAjolote/lambda/src/utilities/languageStrings.js
@@ -12,7 +12,7 @@ module.exports = {
     translation: {
       SKILL_NAME: 'Curiosidades del Ajolote',
       WELCOME_MSG: '¡Hola! bienvenido a Curiosidades del Ajolote, para comenzar puedes decir: dime un dato curioso del ajolote... o si deseas detenerme solo di: ¡Cancela!... entonces, ¿cómo te puedo ayudar?',
-      HELLO_MSG: 'Hola Mundo!',
+      HELLO_MSG: '¡Hola Mundo!',
       HELP_MSG: 'Puedes decir: dame un dato curioso del ajolote... o simplemente para detenerme puedes decir: ¡Cancela!... ¿Cómo te puedo ayudar?',
       GOODBYE_MSG: 'Hasta luego, y recuerda el ajolote se encuentra en peligro de extinción.',
       REFLECTOR_MSG: 'Acabas de activar {{intentName}}',
@@ -24,16 +24,16 @@ module.exports = {
   },
   en: {
     translation: {
-      SKILL_NAME: 'Curiosidades del Ajolote',
-      WELCOME_MSG: '¡Hola! bienvenido a Curiosidades del Ajolote, para comenzar puedes decir: dime un dato curioso del ajolote... o si deseas detenerme solo di: ¡Cancela!... entonces, ¿cómo te puedo ayudar?',
-      HELLO_MSG: 'Hola Mundo!',
-      HELP_MSG: 'Puedes decir: dame un dato curioso del ajolote... o simplemente para detenerme puedes decir: ¡Cancela!... ¿Cómo te puedo ayudar?',
-      GOODBYE_MSG: 'Hasta luego, y recuerda el ajolote se encuentra en peligro de extinción.',
-      REFLECTOR_MSG: 'Acabas de activar {{intentName}}',
-      FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
-      ERROR_MSG: 'Lo siento, ha habido un error. Por favor inténtalo otra vez.',
+      SKILL_NAME: 'Curiosities of the Axolotl',
+      WELCOME_MSG: 'Hello! welcome to Curiosities of the Axolotl, to start you can say: tell me a curious fact about the axolotl ... or if you want to stop me just say: Cancel! ... then how can I help you?',
+      HELLO_MSG: 'Hello World!',
+      HELP_MSG: 'You can say: give me a curious fact about the axolotl ... or simply to stop me you can say: Cancel! ... How can I help you?',
+      GOODBYE_MSG: 'Goodbye, and remember the axolotl is in danger of extinction.',
+      REFLECTOR_MSG: 'You just activated {{intentName}}',
+      FALLBACK_MSG: 'Sorry, I do not know anything about that. Please try again.',
+      ERROR_MSG: 'Sorry, there was an error. Please try again.',
 
-      GET_FACT_MESSAGE: 'Un dato curioso es que '
+      GET_FACT_MESSAGE: 'A curious fact is that '
     },
   },
 };
--- Ch_6/CuriosidadesdelAjolote/lambda/src/utilities/languageStrings.js
+++ Ch_6/CuriosidadesdelAjolote/lambda/src/utilities/languageStrings.js
@@ -12,7 +12,7 @@ module.exports = {
     translation: {
       SKILL_NAME: 'Curiosidades del Ajolote',
       WELCOME_MSG: '¡Hola! bienvenido a Curiosidades del Ajolote, para comenzar puedes decir: dime un dato curioso del ajolote... o si deseas detenerme solo di: ¡Cancela!... entonces, ¿cómo te puedo ayudar?',
-      HELLO_MSG: 'Hola Mundo!',
+      HELLO_MSG: '¡Hola Mundo!',
       HELP_MSG: 'Puedes decir: dame un dato curioso del ajolote... o simplemente para detenerme puedes decir: ¡Cancela!... ¿Cómo te puedo ayudar?',
       GOODBYE_MSG: 'Hasta luego, y recuerda el ajolote se encuentra en peligro de extinción.',
       REFLECTOR_MSG: 'Acabas de activar {{intentName}}',
@@ -21,25 +21,25 @@ module.exports = {
 
       GET_FACT_MESSAGE: 'Un dato curioso es que ',
       GET_TYPE_AXOLOTL: 'Has seleccionado el tipo de ajolote ',
-      OK_COMPLETED_AXOLOTL: 'Ese ajolote es el mejor del mundo!',
+      OK_COMPLETED_AXOLOTL: '¡Ese ajolote es el mejor del mundo!',
       CONTACT_MESSAGE: 'Puedes obtener mas informacion en ajolote arroba alexa.com'
     },
   },
   en: {
     translation: {
-      SKILL_NAME: 'Curiosidades del Ajolote',
-      WELCOME_MSG: '¡Hola! bienvenido a Curiosidades del Ajolote, para comenzar puedes decir: dime un dato curioso del ajolote... o si deseas detenerme solo di: ¡Cancela!... entonces, ¿cómo te puedo ayudar?',
-      HELLO_MSG: 'Hola Mundo!',
-      HELP_MSG: 'Puedes decir: dame un dato curioso del ajolote... o simplemente para detenerme puedes decir: ¡Cancela!... ¿Cómo te puedo ayudar?',
-      GOODBYE_MSG: 'Hasta luego, y recuerda el ajolote se encuentra en peligro de extinción.',
-      REFLECTOR_MSG: 'Acabas de activar {{intentName}}',
-      FALLBACK_MSG: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
-      ERROR_MSG: 'Lo siento, ha habido un error. Por favor inténtalo otra vez.',
+      SKILL_NAME: 'Curiosities of the Axolotl',
+      WELCOME_MSG: 'Hello! welcome to Curiosities of the Axolotl, to start you can say: tell me a curious fact about the axolotl ... or if you want to stop me just say: Cancel! ... then how can I help you?',
+      HELLO_MSG: 'Hello World!',
+      HELP_MSG: 'You can say: give me a curious fact about the axolotl ... or simply to stop me you can say: Cancel! ... How can I help you?',
+      GOODBYE_MSG: 'Goodbye, and remember the axolotl is in danger of extinction.',
+      REFLECTOR_MSG: 'You just activated {{intentName}}',
+      FALLBACK_MSG: 'Sorry, I do not know anything about that. Please try again.',
+      ERROR_MSG: 'Sorry, there was an error. Please try again.',
 
-      GET_FACT_MESSAGE: 'Un dato curioso es que ',
-      GET_TYPE_AXOLOTL: 'Has seleccionado el tipo de ajolote ',
-      OK_COMPLETED_AXOLOTL: 'Ese ajolote es el mejor del mundo!',
-      CONTACT_MESSAGE: 'Puedes obtener mas informacion en ajolote arroba alexa.com'
+      GET_FACT_MESSAGE: 'A curious fact is that ',
+      GET_TYPE_AXOLOTL: 'You have selected the type of axolotl ',
+      OK_COMPLETED_AXOLOTL: 'That axolotl is the best in the world!',
+      CONTACT_MESSAGE: 'You can get more information on ajolote at alexa.com'
     },
     
   },
