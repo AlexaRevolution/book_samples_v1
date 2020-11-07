7ba47c8b0e6698bea90c0d60decd834c6696969b
Xavi <xavi_tb@hotmail.com>
2020-11-07 18:52:21 +0100

    changes fom the book

--- Ch_10/CuriosidadesdelAjolote_local_i18n/lambda/src/interceptors/savePersistentResponseInterceptor.js
+++ Ch_10/CuriosidadesdelAjolote_local_i18n/lambda/src/interceptors/savePersistentResponseInterceptor.js
@@ -8,11 +8,15 @@ module.exports = {
 
       const { attributesManager, requestEnvelope } = handlerInput;
 
-      console.log(
-        "Saving to persistent storage:" + JSON.stringify(requestEnvelope)
-      );
-      attributesManager.setPersistentAttributes(requestEnvelope);
-      await attributesManager.savePersistentAttributes();
+      const shouldEndSession = typeof response.shouldEndSession === 'undefined' ? true : response.shouldEndSession; //is this a session end?        
+      if (shouldEndSession || Alexa.getRequestType(requestEnvelope) === 'SessionEndedRequest') {
+        console.log(
+          "Saving to persistent storage:" + JSON.stringify(requestEnvelope)
+        );
+        attributesManager.setPersistentAttributes(requestEnvelope);
+        await attributesManager.savePersistentAttributes();
+      }
+
     },
   },
 };
