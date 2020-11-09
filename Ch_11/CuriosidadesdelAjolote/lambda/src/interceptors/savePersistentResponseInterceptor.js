fdcb30e37c31849b7d334a1019209fca14ee8313
Xavi <xavi_tb@hotmail.com>
2020-11-09 13:20:57 +0100

    fix save persistence

--- Ch_11/CuriosidadesdelAjolote/lambda/src/interceptors/savePersistentResponseInterceptor.js
+++ Ch_11/CuriosidadesdelAjolote/lambda/src/interceptors/savePersistentResponseInterceptor.js
@@ -8,12 +8,14 @@ module.exports = {
 
       const { attributesManager, requestEnvelope } = handlerInput;
 
+      const sessionAttributes = attributesManager.getSessionAttributes();
+
       const shouldEndSession = typeof response.shouldEndSession === 'undefined' ? true : response.shouldEndSession; //is this a session end?        
       if (shouldEndSession || Alexa.getRequestType(requestEnvelope) === 'SessionEndedRequest') {
         console.log(
-          "Saving to persistent storage:" + JSON.stringify(requestEnvelope)
+          "Saving to persistent storage:" + JSON.stringify(sessionAttributes)
         );
-        attributesManager.setPersistentAttributes(requestEnvelope);
+        attributesManager.setPersistentAttributes(sessionAttributes);
         await attributesManager.savePersistentAttributes();
       }
 
